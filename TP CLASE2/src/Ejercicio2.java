e41258af3f4014db0ada4a0f76d0458891b1cdad
Silvia Cervantes <silviaccp@gmail.com>
2022-03-25 08:42:21 -0300

    TP2 inicial

---
+++ TP CLASE2/src/Ejercicio2.java
@@ -0,0 +1,14 @@
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//tipo de dato entero
+		int entero=10;
+		//tipo de dato cadena
+		String cadena= "Una cadena";
+		System.out.print(entero);
+		System.out.print(cadena);
+	}
+
+}
