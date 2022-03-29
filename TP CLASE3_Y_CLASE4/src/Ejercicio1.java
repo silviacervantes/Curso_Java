44a53b24e12e0e11fbbe36d66554cc47371bc207
Silvia Cervantes <silviaccp@gmail.com>
2022-03-29 09:05:27 -0300

    Update Ejercicio1.java

--- TP CLASE3_Y_CLASE4/src/Ejercicio1.java
+++ TP CLASE3_Y_CLASE4/src/Ejercicio1.java
@@ -3,9 +3,15 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nombre="Silvia Cervantes";
-		int edad=40;
-		System.out.print("Mi nombre es:" +nombre+ " y mi edad es:" +edad+ "años.");
+		Scanner teclado=new Scanner(System.in);
+		
+		System.out.print("Ingrese su nombre: ");
+		String nombre = teclado.nextString();
+
+		System.out.print("Ingrese edad: ");
+		Scanner edad = teclado.nextInt();
+		
+		System.out.print("Mi nombre es:" +nombre+ " y mi edad es:" +edad+ "aï¿½os.");
 
 	}
 
