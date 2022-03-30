6c0bc58c958b3f73c951185b3364f24c41cfa53c
Silvia Cervantes <silviaccp@gmail.com>
2022-03-30 19:56:18 -0300

    cambios

---
+++ TP CLASE3_Y_CLASE4/src/ejemplo2.java
@@ -0,0 +1,13 @@
+
+public class ejemplo2 {
+
+	public static int multiplicacion(int x, int y) {
+		int resultado = x*y;
+		return resultado;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(multiplicacion(3,6));
+	}
+
+}
